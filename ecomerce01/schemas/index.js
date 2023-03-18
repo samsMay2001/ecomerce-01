58e3b6b7eb31922b225c00b2f4ca363f3a27c303
sam <samscloud2@gmail.com>
2023-03-18 13:41:30 -0300

    create 2 schemas: banner and product

--- ecomerce01/schemas/index.js
+++ ecomerce01/schemas/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import banner from "./banner";
+import product from "./product";
+
+/**
+ * creating schemas is like creating tables in mysql except sanity does everything for you. 
+ * by now you just created a banner schema and a product schema
+ * this line bellow tells sanity the structure of the schemas to be created. 
+ */
+export const schemaTypes = [product, banner]
